4b1f60d49740755b974c3b54c0e11fa60e54fc3b
sm-shakib <smsakib2403@gmail.com>
2023-10-05 13:37:13 +0600

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+//app.use(express.urlencoded({ extended: true}));
+
 app.get("/", (req, res) => {
     return res.send("Hello SM!");
 });
+
+app.post("/api/data", (req, res) => {
+    console.log(req.body);
+
+    return res.sendStatus(200);
+});
+
+app.all("/api/all", (req, res) => {
+    return res.sendStatus(200);
+})
 
 app.listen(3000, () => {
     console.log("Application is listening at http://localhost:3000");
